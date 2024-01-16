6ed5422bc5be8f18465d067464fa0b980bdb9f34
TianxiangR <ren644120622@gmail.com>
2024-01-16 16:19:24 -0700

    feat: implemented mean response time

--- StatisticalRequest.cpp
+++ StatisticalRequest.cpp
@@ -1,11 +1,18 @@
 #include "StatisticalRequest.hpp"
 
-void StatisticalRequest::start(const std::string& uri)
+void StatisticalRequest::start(const std::string &uri)
 {
-    // Do something
+  m_startTime = std::chrono::high_resolution_clock::now();
 }
 
 void StatisticalRequest::finish()
 {
-    // Do something
+  auto endTime = std::chrono::high_resolution_clock::now();
+  m_totalResponseTime += std::chrono::duration_cast<std::chrono::microseconds>(endTime - m_startTime);
+  m_processedRequests++;
+}
+
+int64_t StatisticalRequest::getMeanResponseTime() const
+{
+  return m_processedRequests > 0 ? m_totalResponseTime.count() / m_processedRequests : 0;
 }
--- StatisticalRequest.hpp
+++ StatisticalRequest.hpp
@@ -1,4 +1,5 @@
 #include "Request.hpp"
+#include <chrono>
 
 /**
  * @brief a request class that does statistical analysis: mean response time,
@@ -7,9 +8,15 @@
  */
 class StatisticalRequest : public Request
 {
+private:
+  size_t m_processedRequests = 0;
+  std::chrono::microseconds m_totalResponseTime = std::chrono::microseconds(0);
+  std::chrono::high_resolution_clock::time_point m_startTime;
+
 public:
   StatisticalRequest() : Request() {}
   virtual ~StatisticalRequest() = default;
+  int64_t getMeanResponseTime() const;
 
 protected:
   void start(const std::string &uri) override;
