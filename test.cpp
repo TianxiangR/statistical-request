d4f0b6febce443b9cb6f15d7accc2eb29c2b6b00
TianxiangR <ren644120622@gmail.com>
2024-01-18 18:37:06 -0700

    test: added test cases for histogram feature

--- test.cpp
+++ test.cpp
@@ -12,92 +12,137 @@ using namespace std;
 
 int main()
 {
-  describe("Test Statistics", []()
-           {
-    test("getMean returns 0 when no data has been added", [](){
-      Statistics statistics;
-      assertEqual(statistics.getMean(), 0.0);
-    });
+    describe("Test Statistics", []()
+             {
+        test("getMean returns 0 when no data has been added", [](){
+            Statistics statistics(4);
+            assertEqual(statistics.getMean(), 0.0);
+        });
 
-    test("getMean returns the mean of the data", [](){
-      Statistics statistics;
-      statistics.addData(1.0);
-      statistics.addData(2.0);
-      statistics.addData(3.0);
-      assertEqual(statistics.getMean(), 2.0);
-    }); 
-    
-    test("getStandardDeviation returns 0 when no data has been added", [](){
-      Statistics statistics;
-      assertEqual(statistics.getStandardDeviation(), 0.0);
-    });
+        test("getMean returns the correct mean of the data", [](){
+            Statistics statistics(4);
+            statistics.addData(1.0);
+            statistics.addData(2.0);
+            statistics.addData(3.0);
+            assertEqual(statistics.getMean(), 2.0);
+        }); 
+        
+        test("getStandardDeviation returns 0 when no data has been added", [](){
+            Statistics statistics(4);
+            assertEqual(statistics.getStandardDeviation(), 0.0);
+        });
 
-    test("getStandardDeviation returns the standard deviation of the data", [](){
-      Statistics statistics;
-      statistics.addData(1.0);
-      statistics.addData(2.0);
-      statistics.addData(3.0);
-      assertEqual(statistics.getStandardDeviation(), sqrt(2.0/3));
-    }); });
+        test("getStandardDeviation returns the correct standard deviation of the data", [](){
+            Statistics statistics(4);
+            statistics.addData(1.0);
+            statistics.addData(2.0);
+            statistics.addData(3.0);
+            assertEqual(statistics.getStandardDeviation(), sqrt(2.0/3));
+        }); 
+        
+        test("getNormalizedHistogram returns an empty string when no data has been added", [](){
+            Statistics statistics(4);
+            assertEqual(statistics.getNormalizedHistogram(), string(""));
+        }); 
+        
+        test("getNormalizedHistogram returns the histogram string", [](){
+            Statistics statistics(4);
+            statistics.addData(1.0);
+            statistics.addData(1.0);
+            statistics.addData(1.0);
+            statistics.addData(2.0);
+            statistics.addData(3.0);
+            statistics.addData(3.0);
+            statistics.addData(4.0);
 
-  describe("Test StatisticalRequest", []()
-           {
-    test("getMeanResponseTime returns 0 when no data has been added", []()
-         {
-      StatisticalRequest statisticalRequest;
-      assertEqual(statisticalRequest.getMeanResponseTime("uri"), 0.0); });
+            string histogram = statistics.getNormalizedHistogram();
+            string expectedHistogram = 
+                string("0.00-0.25 | * * * \n") +
+                "0.25-0.50 | * \n" +
+                "0.50-0.75 | * * \n" +
+                "0.75-1.00 | * \n";
 
-    test("getMeanResponseTime returns the mean of the data", []()
-         {
-      StatisticalRequest statisticalRequest;
-      chrono::microseconds startTime = chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now().time_since_epoch());
-      statisticalRequest.process("uri1");
-      statisticalRequest.process("uri1");
-      chrono::microseconds endTime = chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now().time_since_epoch());
-      chrono::microseconds responseTime = endTime - startTime;
+            assertEqual(histogram, expectedHistogram);}); });
 
-      assertTrue(statisticalRequest.getMeanResponseTime("uri1") >= responseTime.count() / 2 - ERROR); });
+    describe("Test StatisticalRequest", []()
+             {
+        test("getMeanResponseTime returns 0 when no data has been added", []()
+            {
+            StatisticalRequest statisticalRequest(4);
+            assertEqual(statisticalRequest.getMeanResponseTime("uri"), 0.0); 
+        });
 
-    test("processing another uri would not affect the mean of the previous uri", [](){
-      StatisticalRequest statisticalRequest;
-      statisticalRequest.process("uri1");
-      double meanResponseTime1 = statisticalRequest.getMeanResponseTime("uri1");
-      statisticalRequest.process("uri2");
-      double meanResponseTime2 = statisticalRequest.getMeanResponseTime("uri1");
+        test("getMeanResponseTime returns the mean of the data", [](){
+            StatisticalRequest statisticalRequest(4);
+            chrono::microseconds startTime = 
+                chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now().time_since_epoch());
+            statisticalRequest.process("uri1");
+            statisticalRequest.process("uri1");
+            chrono::microseconds endTime = 
+                chrono::duration_cast<chrono::microseconds>(chrono::high_resolution_clock::now().time_since_epoch());
+            chrono::microseconds responseTime = endTime - startTime;
 
-      // assert the mean response time of uri1 remains the same before and after processing uri2
-      assertEqual(meanResponseTime1, meanResponseTime2); });
+            assertTrue(statisticalRequest.getMeanResponseTime("uri1") >= responseTime.count() / 2 - ERROR); });
 
-      
-    test("getStandardDeviation returns 0 when no data has been added", [](){
-      StatisticalRequest statisticalRequest;
-      assertEqual(statisticalRequest.getStandardDeviation("uri"), 0.0); }); 
-      
-    test("getStandardDeviation returns the standard deviation of the data", [](){
-      StatisticalRequest statisticalRequest;
-      vector<double> responseTimes;
+            test("processing another uri would not affect the mean of the previous uri", [](){
+            StatisticalRequest statisticalRequest(4);
+            statisticalRequest.process("uri1");
+            double meanResponseTime1 = statisticalRequest.getMeanResponseTime("uri1");
+            statisticalRequest.process("uri2");
+            double meanResponseTime2 = statisticalRequest.getMeanResponseTime("uri1");
 
-      // this test case is based on the trust on getMeanResponseTime, so we need to test getMeanResponseTime thoroughly first
-      // throughout the calculations, we get the response time for each time we process the uri and store it in a vector
-      statisticalRequest.process("uri1");
-      responseTimes.push_back(statisticalRequest.getMeanResponseTime("uri1"));
-      statisticalRequest.process("uri1");
-      responseTimes.push_back(statisticalRequest.getMeanResponseTime("uri1") * 2 - responseTimes[0]);
-      statisticalRequest.process("uri1");
-      responseTimes.push_back(statisticalRequest.getMeanResponseTime("uri1") * 3 - responseTimes[0] - responseTimes[1]);
+            // assert the mean response time of uri1 remains the same before and after processing uri2
+            assertEqual(meanResponseTime1, meanResponseTime2); 
+        });
 
-      // compute standard deviation
-      double mean = statisticalRequest.getMeanResponseTime("uri1");
-      double sum = 0.0;
+        
+        test("getStandardDeviation returns 0 when no data has been added", [](){
+            StatisticalRequest statisticalRequest(4);
+            assertEqual(statisticalRequest.getStandardDeviation("uri"), 0.0); 
+        }); 
+            
+        test("getStandardDeviation returns the standard deviation of the data", [](){
+            StatisticalRequest statisticalRequest(4);
+            vector<double> responseTimes;
 
-      for (auto responseTime : responseTimes)
-      {
-        double diff = responseTime - mean;
-        sum += diff * diff;
-      }
+            // this test case is based on the trust on getMeanResponseTime, 
+            // so we need to test getMeanResponseTime thoroughly first
+            // throughout the calculations, we get the response time for 
+            // each time we process the uri and store it in a vector
+            statisticalRequest.process("uri1");
+            responseTimes.push_back(statisticalRequest.getMeanResponseTime("uri1"));
+            statisticalRequest.process("uri1");
+            responseTimes.push_back(statisticalRequest.
+                getMeanResponseTime("uri1") * 2 - responseTimes[0]);
+            statisticalRequest.process("uri1");
+            responseTimes.push_back(statisticalRequest.
+                getMeanResponseTime("uri1") * 3 - responseTimes[0] - responseTimes[1]);
 
-      double standardDeviation = sqrt(sum / responseTimes.size());
+            // compute standard deviation
+            double mean = statisticalRequest.getMeanResponseTime("uri1");
+            double sum = 0.0;
 
-      assertEqual(statisticalRequest.getStandardDeviation("uri1"), standardDeviation);
-      }); });
+            for (auto responseTime : responseTimes)
+            {
+                double diff = responseTime - mean;
+                sum += diff * diff;
+            }
+
+            double standardDeviation = sqrt(sum / responseTimes.size());
+
+            assertEqual(statisticalRequest.getStandardDeviation("uri1"), standardDeviation);
+        }); 
+        
+        
+        test("getNormalizedHistogram returns an empty string when no data has been added", [](){
+            StatisticalRequest statisticalRequest(4);
+            assertEqual(statisticalRequest.getNormalizedHistogram("uri"), string("")); 
+        }); 
+        
+        test("getNormalizedHistogram returns the histogram string", [](){
+            StatisticalRequest statisticalRequest(4);
+            for (int i = 0; i < 100; i++)
+                statisticalRequest.process("uri1");
+            assertNotEqual(statisticalRequest.getNormalizedHistogram("uri1"), string(""));
+        }); });
 }
