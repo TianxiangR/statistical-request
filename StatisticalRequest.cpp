150ccbb90c9801f19d1045d2f1cc623bdbaf5c86
TianxiangR <ren644120622@gmail.com>
2024-01-18 18:36:17 -0700

    feat: added histogram feature to StatisticalRequest class

--- StatisticalRequest.cpp
+++ StatisticalRequest.cpp
@@ -12,12 +12,13 @@ void StatisticalRequest::finish()
 {
   // record the end time and calculate the response time
   auto endTime = std::chrono::high_resolution_clock::now();
-  auto responseTime = std::chrono::duration_cast<std::chrono::microseconds>(endTime - m_startTime);
+  auto responseTime =
+      std::chrono::duration_cast<std::chrono::microseconds>(endTime - m_startTime);
 
   // add the response time to the statistics for the uri
   if (m_statistics.find(m_uri) == m_statistics.end())
   {
-    m_statistics[m_uri] = Statistics();
+    m_statistics[m_uri] = Statistics(m_max_bins);
     m_statistics[m_uri].addData(responseTime.count());
   }
   else
@@ -38,4 +39,12 @@ double StatisticalRequest::getStandardDeviation(const std::string &uri) const
   if (m_statistics.find(uri) == m_statistics.end())
     return 0.0;
   return m_statistics.at(uri).getStandardDeviation();
+};
+
+std::string StatisticalRequest::getNormalizedHistogram(const std::string &uri) const
+{
+  if (m_statistics.find(uri) == m_statistics.end())
+    return "";
+
+  return m_statistics.at(uri).getNormalizedHistogram();
 };
